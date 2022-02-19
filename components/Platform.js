147f74741ee7a520f27afa7bb9ed761b7519eb18
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-02-19 23:08:21 +0100

    create platform and import files

---
+++ components/Platform.js
@@ -0,0 +1,18 @@
+const Platform = () => {
+  return (
+    <div className="body">
+      <div className="body-wrap">
+        <div className="edv">
+          <div className="edv-wrap">
+            <div className="left">
+              <h1>Edvora</h1>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Platform;
