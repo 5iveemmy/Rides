7e0f3c59d0f4b07cd01e063bb83ca4a50c04b908
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-02-20 00:06:14 +0100

    import styled component

--- components/Platform.js
+++ components/Platform.js
@@ -1,21 +1,44 @@
 import Image from "next/image";
+import styled from "styled-components";
+
+// STyled components
+const Body = styled.div`
+  background-color: #101010;
+  color: #fff;
+`;
+
+const BodyWrap = styled.div``;
+
+const EdvWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Edv = styled.div``;
+
+const Left = styled.div``;
+
+const LeftText = styled.h1``;
+
+const Right = styled.div``;
 
 const Platform = () => {
   return (
-    <div className="body">
-      <div className="body-wrap">
-        <div className="edv">
-          <div className="edv-wrap">
-            <div className="left">
-              <h1>Edvora</h1>
-            </div>
-            <div className="right">
+    <Body>
+      <BodyWrap>
+        <Edv>
+          <EdvWrap>
+            <Left>
+              <LeftText>Edvora</LeftText>
+            </Left>
+            <Right>
               <Image src="/avi.png" alt="avatar" width={44} height={44} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </Right>
+          </EdvWrap>
+        </Edv>
+      </BodyWrap>
+    </Body>
   );
 };
 
